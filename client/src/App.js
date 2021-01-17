f0892931a1343e68b7924ecc075cca64883b091e
Khuong <lekhuongy@gmail.com>
2021-01-17 16:43:31 -0600

    working search feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {NavBar, AllImages} from './components'
 import './styles/app.css'
+import axios from 'axios'
 
-function App () {
-  return (
-    <div>
-      <NavBar />
-      <AllImages />
-    </div>
-  )
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state = {images: [], filter: '', filteredImage : []}
+    this.setFilter = this.setFilter.bind(this)
+    this.onSubmitFilter = this.onSubmitFilter.bind(this) 
+  }
+
+  componentDidMount () {
+    this.callApi()
+      .then(res => this.setState({images: res, filteredImage: res}))
+      .catch(err => console.log(err))
+  }
+
+  async callApi () {
+    const res = await axios.get('/api')
+    const body = await res.data
+    if(res.status !== 200) throw Error(body.message)
+    return body
+  }
+
+  setFilter (evt) {
+    evt.preventDefault()
+    const lowercaseName = evt.target.value.toLowerCase()
+    this.setState({filter: lowercaseName})
+  }
+
+  onSubmitFilter(evt) {
+    evt.preventDefault()
+    const {images, filter} = this.state
+    const filtered = images.filter(currImg => currImg.name.toLowerCase().includes(filter))
+    this.setState({filteredImage: filtered, filter: ''})
+  }
+
+  render () {
+    return (
+      <div>
+        <NavBar filter={this.state.filter} setFilter={this.setFilter} onSubmitFilter={this.onSubmitFilter}/>
+        <AllImages images={this.state.filteredImage}/>
+      </div>
+    )
+  }
 }
 
 export default App;
--- client/src/components/allImages.js
+++ client/src/components/allImages.js
@@ -1,36 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import SingleImage from './singleImg';
-import axios from 'axios'
 
+function Images (props) {
+  const {images} = props
 
-class Images extends Component {
-
-  state = {
-    images : []
-  }
-
-  componentDidMount () {
-    this.callApi()
-      .then(res => this.setState({images: res}))
-      .catch(err => console.log(err))
-  }
-
-  async callApi () {
-    const res = await axios.get('/api')
-    const body = await res.data
-    if(res.status !== 200) throw Error(body.message)
-    return body
-  }
-
-  render () {
-    const {images} = this.state
     return (
       <section className="flex flex-row flex-wrap p-5">
         {images.map(currImg => <SingleImage info={currImg} key={currImg.id}/>)}
       </section>
     );
 
-  }
 }
 
 export default Images;
